68961c3d9b81484693cbf8c01d8ef28ec5f5c6bc
JSPractice <maxelvis512@gmail.com>
2025-01-20 22:52:37 +0100

    Use product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,30 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceInCent: 1090,
-  },{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceInCent: 2095,
-  },{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceInCent: 799,
-  },
-]
-
 let totalHtml = '';
 const productGrid = document.querySelector('.js-product-grid')
 products.forEach((product) => {
@@ -48,7 +21,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ${(product.priceInCent / 100).toFixed(2)}
+            ${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -80,4 +53,3 @@ products.forEach((product) => {
   `;
 })
 productGrid.innerHTML = totalHtml;
-console.log(totalHtml);
