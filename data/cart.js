56da1f2d9fdfd8dbbb6bedf54c10d3e080268fe0
JSPractice <maxelvis512@gmail.com>
2025-01-23 19:54:52 +0100

    saving cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,24 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1,
-}, {
-  productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-  quantity: 3,
-}
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
+  {
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  },
 
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+  },
+  
+  {
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity: 3,
+  }
 ];
+
+
+function saveCartInLocalStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId, quantity) {
   let matchingItem;
@@ -28,9 +37,11 @@ export function addToCart(productId, quantity) {
       quantity,
     });
   }
+  saveCartInLocalStorage();
 }
 
 export function removeItem(productToDeleteId) {
-let newCart = cart.filter((cartItem) => cartItem.productId !== productToDeleteId);
+  let newCart = cart.filter((cartItem) => cartItem.productId !== productToDeleteId);
   cart = newCart;
+  saveCartInLocalStorage();
 }
