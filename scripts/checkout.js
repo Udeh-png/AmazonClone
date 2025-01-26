1be1bd5a843eeafe2ec3c12d22afa145c3a497a5
JSPractice <maxelvis512@gmail.com>
2025-01-26 17:14:41 +0100

    Save data and generate html fot delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import { cart, removeItem, updateQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import { deliveryOptions } from "../data/deliveryOptions.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 let totalHtml = '';
 cart.forEach((cartItem) => {
@@ -11,10 +13,20 @@ cart.forEach((cartItem) => {
     }
   });
 
+  let matchingDeliveryOption;
+  deliveryOptions.forEach((deliveryOption) => {
+    if (cartItem.deliveryOptionId === deliveryOption.id) {
+      matchingDeliveryOption = deliveryOption;
+    }
+  });
+    const today = dayjs();
+    const addToToday = today.add(matchingDeliveryOption.deliveryDate, 'Days');
+    const formatAddToToday = addToToday.format('dddd, MMMM D');
+
   totalHtml += `
     <div class="cart-item-container js-cart-item-container-${cartItem.productId}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${formatAddToToday}
       </div>
 
       <div class="cart-item-details-grid">
@@ -51,50 +63,45 @@ cart.forEach((cartItem) => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio" checked
-              class="delivery-option-input"
-              name="delivery-option-${cartItem.productId}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${cartItem.productId}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${cartItem.productId}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${generateDeliveryOptionHTML(cartItem)}
+          
         </div>
       </div>
     </div>`;
 });
 document.querySelector('.js-order-summary').innerHTML = totalHtml;
+
+function generateDeliveryOptionHTML(cartItem) {
+  let deliveryOptionHtml = '';
+  let priceString;
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs();
+    const addToToday = today.add(deliveryOption.deliveryDate, 'Days');
+    const formatAddToToday = addToToday.format('dddd, MMMM D');
+    const isChecked = cartItem.deliveryOptionId === deliveryOption.id;
+    
+    deliveryOptionHtml += `
+    <div class="delivery-option">
+      <input type="radio"
+        ${isChecked ? 'checked' : ''}
+        class="delivery-option-input"
+        name="delivery-option-${cartItem.productId}">
+      <div>
+        <div class="delivery-option-date">
+          ${formatAddToToday}
+        </div>
+        <div class="delivery-option-price">
+          ${priceString = 
+            deliveryOption.priceCents === 0 
+            ? 'FREE' 
+            :`$${formatCurrency(deliveryOption.priceCents)} -`} Shipping
+        </div>
+      </div>
+    </div>
+  `
+  })
+  return deliveryOptionHtml;
+}
 
 document.querySelectorAll('.js-delete-quantity-link')
   .forEach((deleteButton) => {
