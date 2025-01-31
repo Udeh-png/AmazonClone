f28929dfe649fff8b99ebb1950b6e47d9cffdb51
JSPractice <maxelvis512@gmail.com>
2025-01-31 13:00:00 +0100

    remove default tests from jasmine and add money test

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe('test suite: formatCurrency', () => {
+  it('works with whole numbers', () => {
+    expect(formatCurrency(1234)).toEqual('12.34')
+  })
+
+  it('works with 0', () => {
+    expect(formatCurrency(0)).toEqual('0.00')
+  })
+
+  it('can round up', () => {
+    expect(formatCurrency(2009.89)).toEqual('20.10')
+  })
+})
+
---
+++ tests/moneyTest.js
@@ -0,0 +1,31 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log('test suite: formatCurrency');
+
+console.log(' works with whole numbers');
+if (formatCurrency(1229) === '12.29') {
+  console.log(' passed');
+}else{
+  console.log(' failed');
+}
+
+console.log(' works with 0');
+if (formatCurrency(0) === '0.00') {
+  console.log(' passed');
+}else{
+  console.log(' failed');
+}
+
+console.log(' can round up');
+if (formatCurrency(2000.5) === '20.01') {
+  console.log(' passed');
+}else{
+  console.log(' failed');
+}
+
+console.log(' can round down');
+if (formatCurrency(2000.4) === '20.00') {
+  console.log(' passed');
+}else{
+  console.log(' failed');
+}
