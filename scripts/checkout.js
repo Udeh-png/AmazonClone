72bb87f1129c306dc036bccca9f929affdf57cd2
JSPractice <maxelvis512@gmail.com>
2025-02-18 21:52:48 +0100

    Learn Promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,34 @@
 import renderOrderSummary from "./checkout/orderSummary.js";
 import renderPaymentSummary from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/backend-practice.js";
-
-loadProducts(() => {
+Promise.all([new Promise((reserve) => {
+  loadProducts(() => {
+    reserve();
+  })
+}), new Promise(( reserve) => {
+  loadCart(() => {
+    reserve();
+  })
+})]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve()
+    })
+  });
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
