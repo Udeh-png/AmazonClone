eca279d187166cbd823a3ad9b0a6e5f1acdead49
devs-java <devsjava770@gmail.com>
2025-04-16 17:20:50 +0100

    fix: correct import path and format delivery options for consistency

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,23 +1,23 @@
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import isWeekend from "../../scripts/utils/No-Weekend.js";
+import isWeekend from "../scripts/utils/No-Weekend.js";
 
 export const deliveryOptions = [
-{
-  id: '1',
-  deliveryDate: 7,
-  priceCents: 0,
-},
-{
-  id: '2',
-  deliveryDate: 3,
-  priceCents: 499,
-},
-{
-  id: '3',
-  deliveryDate: 1,
-  priceCents: 999,
-}
-]
+  {
+    id: "1",
+    deliveryDate: 7,
+    priceCents: 0,
+  },
+  {
+    id: "2",
+    deliveryDate: 3,
+    priceCents: 499,
+  },
+  {
+    id: "3",
+    deliveryDate: 1,
+    priceCents: 999,
+  },
+];
 
 export function getDeliveryOptionWithId(deliveryOptionId) {
   let matchingDeliveryOption;
@@ -36,12 +36,12 @@ export function calculateDeliveryDate(cartDaysToAdd, startDate) {
   let count = 0;
 
   while (count < daysToAdd) {
-    today = today.add(1, 'Days');
-    const weekendCheck = today.format('dddd');
+    today = today.add(1, "Days");
+    const weekendCheck = today.format("dddd");
 
     if (!isWeekend(weekendCheck)) {
       count++;
     }
   }
-  return today.format('dddd, MMMM D');
+  return today.format("dddd, MMMM D");
 }
